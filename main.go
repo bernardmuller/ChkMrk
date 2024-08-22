4476d801c76a955b4df83b97b518603a06ae5267
Bernard Muller <b.mullerjnr@gmail.com>
2024-08-22 18:32:58 +0200

    feat: print a list of items

--- main.go
+++ main.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
+type Item struct {
+	Completed bool
+	Name      string
+}
+
+var list []Item
+
+func RenderList(list []Item) {
+	for i := 0; i < len(list); i++ {
+		fmt.Println(list[i].Name)
+	}
+}
+
 func main() {
-	fmt.Println("This is a Checklist!")
+	list = append(list, Item{Completed: false, Name: "make a list of items"})
+	list = append(list, Item{Completed: false, Name: "check an item"})
+	list = append(list, Item{Completed: false, Name: "uncheck an item"})
+	list = append(list, Item{Completed: false, Name: "add a new item"})
+	list = append(list, Item{Completed: false, Name: "remove an item"})
+	RenderList(list)
 }
