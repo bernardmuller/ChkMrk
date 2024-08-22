843fdf3820433fd271f38880e8f99634d8e5949d
Bernard Muller <b.mullerjnr@gmail.com>
2024-08-22 19:18:51 +0200

    feat: parse cli comands

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
-	"bytes"
+	"flag"
 	"fmt"
 	"io"
+)
+
+var (
+	intFlag  int
+	strFlag  string
+	boolFlag bool
 )
 
 type Item struct {
@@ -38,6 +44,16 @@ func main() {
 	list = append(list, Item{Completed: false, Name: "test uncheck an item"})
 	list = append(list, Item{Completed: false, Name: "test add a new item"})
 	list = append(list, Item{Completed: false, Name: "test remove an item"})
-	var buffer bytes.Buffer
-	RenderList(&buffer, list)
+	// var buffer bytes.Buffer
+
+	flag.IntVar(&intFlag, "int", 1234, "help message")
+	flag.StringVar(&strFlag, "str", "default", "help message")
+	flag.BoolVar(&boolFlag, "bool", false, "help message")
+	flag.Parse()
+
+	fmt.Println("intFlag value is: ", intFlag)
+	fmt.Println("strFlag value is: ", strFlag)
+	fmt.Println("boolFlag value is: ", boolFlag)
+
+	// RenderList(&buffer, list)
 }
