eee242d4236e32b3ae8479e14232e12cf1a953e8
Bernard Muller <b.mullerjnr@gmail.com>
2024-08-22 19:34:23 +0200

    feat: parse add flag

--- main.go
+++ main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"io"
 )
 
 var (
-	intFlag  int
-	strFlag  string
-	boolFlag bool
+	addFlag string
 )
 
 type Item struct {
@@ -39,21 +38,25 @@ func main() {
 	list = append(list, Item{Completed: true, Name: "test functions that render items"})
 	list = append(list, Item{Completed: false, Name: "parse cli entrypoint without args"})
 	list = append(list, Item{Completed: false, Name: "parse cli entrypoint with args"})
+	list = append(list, Item{Completed: false, Name: "parse add item flag"})
+	list = append(list, Item{Completed: false, Name: "parse check item flag"})
+	list = append(list, Item{Completed: false, Name: "parse remove item flag"})
 	list = append(list, Item{Completed: false, Name: "list args"})
 	list = append(list, Item{Completed: false, Name: "test check an item"})
 	list = append(list, Item{Completed: false, Name: "test uncheck an item"})
 	list = append(list, Item{Completed: false, Name: "test add a new item"})
 	list = append(list, Item{Completed: false, Name: "test remove an item"})
-	// var buffer bytes.Buffer
 
-	flag.IntVar(&intFlag, "int", 1234, "help message")
-	flag.StringVar(&strFlag, "str", "default", "help message")
-	flag.BoolVar(&boolFlag, "bool", false, "help message")
+	flag.StringVar(&addFlag, "a", "default", "help message")
 	flag.Parse()
 
-	fmt.Println("intFlag value is: ", intFlag)
-	fmt.Println("strFlag value is: ", strFlag)
-	fmt.Println("boolFlag value is: ", boolFlag)
+	var buffer bytes.Buffer
+	RenderList(&buffer, list)
 
-	// RenderList(&buffer, list)
+	if addFlag == "default" {
+		fmt.Print(buffer.String())
+	} else {
+		fmt.Println("addFlag value is: ", addFlag)
+	}
+
 }
